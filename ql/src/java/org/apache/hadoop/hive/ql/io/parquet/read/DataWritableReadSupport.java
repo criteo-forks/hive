1c102c25716548f80c0b31a2b71055289d6b500a
Miguel Liroz <m.liroz@criteo.com>
2020-06-03 09:39:52 +0200

    Keep field index within nested fields for schema evolution

--- ql/src/java/org/apache/hadoop/hive/ql/io/parquet/read/DataWritableReadSupport.java
+++ ql/src/java/org/apache/hadoop/hive/ql/io/parquet/read/DataWritableReadSupport.java
@@ -412,6 +412,9 @@ public class DataWritableReadSupport extends ReadSupport<ArrayWritable> {
       metadata.put(key, String.valueOf(HiveConf.getBoolVar(
         configuration, HiveConf.ConfVars.HIVE_PARQUET_TIMESTAMP_SKIP_CONVERSION)));
     }
+    key = HiveConf.ConfVars.HIVE_STRUCT_SCHEMA_CONVERSION_BY_NAME.varname;
+    metadata.put(key, String.valueOf(HiveConf.getBoolVar(
+            configuration, HiveConf.ConfVars.HIVE_STRUCT_SCHEMA_CONVERSION_BY_NAME)));
     return new DataWritableRecordConverter(readContext.getRequestedSchema(), metadata, hiveTypeInfo);
   }
 }
