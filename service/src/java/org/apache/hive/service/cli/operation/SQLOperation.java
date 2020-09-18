difin/difin_hive

HIVE-17300: WebUI query plan graphs (Karen Coppage, reviewed by Szehon Ho and Peter Vary)

service/src/java/org/apache/hive/service/cli/operation/SQLOperation.java
@@ -184,6 +184,9 @@ public class SQLOperation extends ExecuteStatementOperation {
       }
 
       queryInfo.setQueryDisplay(driver.getQueryDisplay());
+      if (operationLog != null) {
+        queryInfo.setOperationLogLocation(operationLog.toString());
+      }
 
       // set the operation handle information in Driver, so that thrift API users
       // can use the operation handle they receive, to lookup query information in

criteo-forks/hive

service/src/java/org/apache/hive/service/cli/operation/SQLOperation.java
@@ -189,6 +189,9 @@ public class SQLOperation extends ExecuteStatementOperation {
       }
 
       sqlOpDisplay.setQueryDisplay(driver.getQueryDisplay());
+      if (operationLog != null) {
+        sqlOpDisplay.setOperationLogLocation(operationLog.toString());
+      }
 
       // set the operation handle information in Driver, so that thrift API users
       // can use the operation handle they receive, to lookup query information in